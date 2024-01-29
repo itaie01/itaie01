c1cb8c14b2963538e616340dadcfb30a1f5ed8e5
itaie <itainepstein@gmail.com>
2024-01-29 10:13:36 -0500

    finished the initial tutorial

--- personal-website/astro.config.mjs
+++ personal-website/astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://itaie.netlify.app/"
+  site: "https://itaie.netlify.app/",
+  integrations: [preact()]
 });
